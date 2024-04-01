29b01581b67025d2006143b94e4823b0f224e882
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-31 22:32:33 -0300

    strings

--- ex004/index.php
+++ ex004/index.php
@@ -7,8 +7,38 @@
 </head>
 <body>
     <?php 
+        // concatenação
         
+        $curso = "Curso";
+        $PHP = "PHP";
 
+        $info = "$curso de $PHP \u{1F418} eh bom"; 
+
+        print($info);
+
+        const ESTADO = "MG";
+
+        print("<br> Mono em ESTADO");
+        print("<br> Mono em " . ESTADO);
+
+        $data = date('Y');
+
+        print("<br> Estamos no ano de $data");
+
+        $nom = "Rodrigo";
+        $snom = "Nogueira";
+
+        print("<br>$nom \"Minotauro\" $snom");
+
+        echo <<< OXI
+            <br> Pensei q fosse uma template string :ccc <br>
+            Assim... eh uma template string, so n funfa pra HTML
+            <br>
+            <br>
+            $nom "Minotauro" $snom
+        OXI;
+
+        
     ?>
 </body>
 </html>
