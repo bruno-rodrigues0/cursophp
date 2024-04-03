fed6c3efea9c3aafc36935044152d31473660547
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-03 09:28:11 -0300

    desafio 03

---
+++ desafios/desafio03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP 03</title>
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+
+    <div>
+        <h1>Coversor de Moedas v1.0</h1>
+        <form action="convert.php" method="get">
+            <label for="value">Quantos reais você tem na carteira</label>
+            <input type="number" name="value" id="value" step="any">
+            <input type="submit" value="Converter">
+        </form>
+    </div>
+</body>
+</html>
