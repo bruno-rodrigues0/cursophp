1fa35753ab48e3d5a613bf0beb6d0b70fff94727
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-03 09:05:07 -0300

    desafio 02

---
+++ desafios/desafio02/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP 02</title>
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+    <?php 
+        function gerarRandom(){
+            $numero = random_int(0, 100);
+            return $numero;
+        }
+    ?>
+
+    <div>
+        <h1>Trabalhando com números aleatórios</h1>
+
+        <p>Gerando um número aleatório entre 0 e 100...</p>
+        <p>O valro gerado foi <strong><?php echo gerarRandom()?></strong></p>
+
+        <a href=""><button>Gerar outro numero</button></a>
+    </div>
+</body>
+</html>
